847b3bf01fb394783e9f544c04915deab11284e1
JIHYUNG KIM <martinjhkim7493@gmail.com>
2026-06-26 17:13:38 +0900

    init

--- js/common/auth.js
+++
@@ -1,4 +0,0 @@
-const TOKEN_KEY = 'any-talk-access-token';
-export const getAccessToken = () => localStorage.getItem(TOKEN_KEY);
-export const setAccessToken = (token) => localStorage.setItem(TOKEN_KEY, token);
-export const logout = () => localStorage.removeItem(TOKEN_KEY);
